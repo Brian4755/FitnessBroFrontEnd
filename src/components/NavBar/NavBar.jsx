e012b32c37649a6f53e001fae2d02827416a054c
Brian4755 <brian.mccune1@yahoo.com>
2023-01-23 16:04:27 -0600

    navbar fix

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,18 +9,18 @@ const NavBar = ({ user, handleLogout }) => {
   return (
     <>
     {user ?
-      <Navbar  variant="dark" expand="lg">
+      <Navbar variant="dark" expand="lg">
         <Container>
           <Navbar.Brand className='brand'>Fitness <span>Bros</span></Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Link to="/" className="nav-link active" aria-current="page">Home</Link>
-              <Link to="/workouts" className="nav-link">Workouts</Link>
-              <Link to="/meals" className="nav-link">Meals</Link>
-              <Link to="/profiles" className="nav-link">Community</Link>
-              <Link to={`/profiles/${user.profile}`} className="nav-link">Account</Link>
-              <Link className="nav-link" to="/" onClick={handleLogout}>Log Out</Link>
+              <Nav.Link href="/" className="nav-link active" aria-current="page">Home</Nav.Link>
+              <Nav.Link href="/workouts" className="nav-link">Workouts</Nav.Link>
+              <Nav.Link href="/meals" className="nav-link">Meals</Nav.Link>
+              <Nav.Link href="/profiles" className="nav-link">Community</Nav.Link>
+              <Nav.Link href={`/profiles/${user.profile}`} className="nav-link">Account</Nav.Link>
+              <Nav.Link className="nav-link" to="/" onClick={handleLogout}>Log Out</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
@@ -32,10 +32,10 @@ const NavBar = ({ user, handleLogout }) => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-            <Link to="/" className="nav-link active" aria-current="page">Home</Link>
-              <Link to="/workouts" className="nav-link">Workouts</Link>
-              <Link to="/meals" className="nav-link">Meals</Link>
-              <Link to="/profiles" className="nav-link">Community</Link>
+            <Nav.Link href="/" className="nav-link active" aria-current="page">Home</Nav.Link>
+              <Nav.Link href="/workouts" className="nav-link">Workouts</Nav.Link>
+              <Nav.Link href="/meals" className="nav-link">Meals</Nav.Link>
+              <Nav.Link href="/profiles" className="nav-link">Community</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
